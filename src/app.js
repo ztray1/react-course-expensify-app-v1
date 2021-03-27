8ac3a80d8e67a98ec99be48ff48d0c269921a057
ztray1 <zhangtuo332@gmail.com>
2021-03-27 11:20:15 -0400

    Reomve dummy expense data

--- src/app.js
+++ src/app.js
@@ -12,21 +12,7 @@ import "react-dates/lib/css/_datepicker.css";
 
 
 const store=configureStore();
-store.dispatch(addExpense({description:"water bill",amount:200,createdAt:12}));
-store.dispatch(addExpense({description:"gas bill",amount:105,createdAt:250}));
-//store.dispatch(setTextFilter("bill"));
 
-const state=store.getState();
-
-const visibleExpenses=getVisibleExpenses(state.expenses,state.filters);
-console.log(visibleExpenses);
-
-/*store.subscribe(()=>{
-    const state=store.getState();
-    const visibleExpenses=getVisibleExpenses(state.expenses,state.filters);
-    console.log(visibleExpenses);
-    console.log(state.filters.sortBy);
-})*/
 
 const jsx=(
     <Provider store={store}>

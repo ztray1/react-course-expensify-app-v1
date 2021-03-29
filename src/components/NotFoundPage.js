891e6eb750770c88d9dd0ad22a922976f5b1009a
ztray1 <zhangtuo332@gmail.com>
2021-03-29 11:45:39 -0400

    NotFoundPage

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,9 +1,10 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
-const notFoundPage=()=>(
+const NotFoundPage=()=>(
     <div>
         404 - <Link to="/">Go Home</Link>
     </div>
 );
 
-export default notFoundPage;
+export default NotFoundPage;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,7 +3,7 @@ import React from "react";
 import AddExpensePage from "../components/AddExpensePage";
 import EditExpensePage from "../components/EditExpensePage";
 import HelpPage from "../components/HelpPage";
-import notFoundPage from "../components/NotFoundPage";
+import NotFoundPage from "../components/NotFoundPage";
 import ExpenseDashBoardPage from "../components/ExpenseDashBoardPage";
 import Header from "../components/Header";
 
@@ -16,7 +16,7 @@ const AppRouter=()=>(
                 <Route path="/create" component={AddExpensePage}/>
                 <Route path="/edit/:id" component={EditExpensePage}/>
                 <Route path="/help" component={HelpPage}/>
-                <Route component={notFoundPage}/>
+                <Route component={NotFoundPage}/>
             </Switch>
         </div>
     </BrowserRouter>
